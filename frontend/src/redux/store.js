78974311a28c4192e357cef0fcc1831c6ce36756
MrKenobii <anldygc35@windowslive.com>
2023-03-29 19:39:48 +0300

    Redux Persist Encrpyt

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,6 +2,8 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import appStateSlice from "./features/appStateSlice";
 import authModalSlice from "./features/authModalSlice";
 import globalLoadingSlice from "./features/globalLoadingSlice";
+import { encryptTransform } from 'redux-persist-transform-encrypt';
+import storageSession from 'redux-persist/es/storage/session'
 import userSlice from "./features/userSlice";
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
@@ -10,6 +12,14 @@ import thunk from 'redux-thunk';
 const persistConfig = {
   key: 'root',
   storage,
+  transforms: [
+    encryptTransform({
+      secretKey: 'my-super-secret-key',
+      onError: function (error) {
+        // Handle the error.
+      },
+    }),
+  ],
 }
 
 const rootReducer = combineReducers({
@@ -18,6 +28,7 @@ const rootReducer = combineReducers({
   // globalLoading: globalLoadingSlice,
   // appState: appStateSlice
 })
+
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
